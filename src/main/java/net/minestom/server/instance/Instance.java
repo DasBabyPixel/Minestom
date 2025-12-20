dd1ea9a1ddbfaf413b948d9eb61d4b2328c455f0
Lorenz Wrobel <43410952+DasBabyPixel@users.noreply.github.com>
2025-12-20 21:07:44 +0100

    wip lighting

--- src/main/java/net/minestom/server/instance/Instance.java
+++ src/main/java/net/minestom/server/instance/Instance.java
@@ -1072,8 +1072,8 @@ public abstract class Instance implements Block.Getter, Block.Setter,
         int coordY = CoordConversion.globalToSectionRelative(blockY);
         int coordZ = CoordConversion.globalToSectionRelative(blockZ);
 
-        if (light.requiresUpdate())
-            LightingChunk.relightSection(chunk.getInstance(), chunk.chunkX, sectionCoordinate, chunk.chunkZ);
+        if (light.requiresUpdate() && chunk instanceof LightingChunk lighting)
+            lighting.relightSection(sectionCoordinate);
         return light.getLevel(coordX, coordY, coordZ);
     }
 
@@ -1088,8 +1088,8 @@ public abstract class Instance implements Block.Getter, Block.Setter,
         int coordY = CoordConversion.globalToSectionRelative(blockY);
         int coordZ = CoordConversion.globalToSectionRelative(blockZ);
 
-        if (light.requiresUpdate())
-            LightingChunk.relightSection(chunk.getInstance(), chunk.chunkX, sectionCoordinate, chunk.chunkZ);
+        if (light.requiresUpdate() && chunk instanceof LightingChunk lighting)
+            lighting.relightSection(sectionCoordinate);
         return light.getLevel(coordX, coordY, coordZ);
     }
 }
